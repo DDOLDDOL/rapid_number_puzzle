1f6e6f3552523022b6be54ed32d3da38c12ccf5f
DDOLDDOL <doyt@naver.com>
2025-09-07 02:10:55 +0900

    fix: Fix number puzzle bug

--- lib/number_puzzle/cubits/number_puzzle_cubit.dart
+++ lib/number_puzzle/cubits/number_puzzle_cubit.dart
@@ -32,33 +32,6 @@ class NumberPuzzleCubit extends Cubit<NumberPuzzleState> {
     );
 
     _startTimer();
-
-    // final tiles = numbers.asMap().entries.map((entry) {
-    //   return NumberTile(
-    //     id: entry.key,
-    //     number: entry.value,
-    //     row: entry.key ~/ gridSize,
-    //     col: entry.key % gridSize,
-    //     visible: true,
-    //   );
-    // }).toList();
-    //
-    // final startTime = DateTime.now();
-    //
-    // emit(
-    //   state.copyWith(
-    //     gridSize: gridSize,
-    //     tiles: tiles,
-    //     nextNumber: 1,
-    //     startTime: startTime,
-    //     elapsedTime: Duration.zero,
-    //     gameStatus: const GameStatus.playing(),
-    //     removingTiles: {},
-    //     pressedTiles: {},
-    //   ),
-    // );
-    //
-    // _startTimer();
   }
 
   void resetGame() {
@@ -68,43 +41,10 @@ class NumberPuzzleCubit extends Cubit<NumberPuzzleState> {
 
   /// 타일을 탭했을 때의 이벤트를 감지합니다
   void pressNumberTile(int number) {
-    if(number == state.boardSize * state.boardSize) _clearTimer();
-    if(number == state.pressedNumber + 1) emit(state.copyWith(pressedNumber: number));
+    if(number != state.pressedNumber + 1) return;
 
-    // final gameStatus = state.gameStatus;
-    //
-    // // 게임이 진행 중이고, 타일이 보이고, 올바른 순서인지 확인
-    // if (gameStatus is! _Playing || !tile.visible || tile.number != state.nextNumber) {
-    //   return;
-    // }
-    //
-    // // 타일 제거 애니메이션 시작
-    // emit(state.copyWith(removingTiles: {...state.removingTiles, tile.id}));
-    //
-    // // 200ms 후 실제 타일 제거
-    // Future.delayed(const Duration(milliseconds: 200), () {
-    //   if (!isClosed) {
-    //     final updatedTiles = state.tiles
-    //         .map((t) => t.id == tile.id ? t.copyWith(visible: false) : t)
-    //         .toList();
-    //
-    //     final nextNumber = state.nextNumber + 1;
-    //     final totalTiles = state.gridSize * state.gridSize;
-    //
-    //     // 게임 완료 확인
-    //     if (nextNumber > totalTiles) {
-    //       completeGame();
-    //     } else {
-    //       emit(
-    //         state.copyWith(
-    //           tiles: updatedTiles,
-    //           nextNumber: nextNumber,
-    //           removingTiles: state.removingTiles.where((id) => id != tile.id).toSet(),
-    //         ),
-    //       );
-    //     }
-    //   }
-    // });
+    if(number == state.boardSize * state.boardSize) _clearTimer();
+    emit(state.copyWith(pressedNumber: number));
   }
 
   void _startTimer() {
