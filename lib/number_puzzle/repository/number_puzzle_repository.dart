07da7b3dc7768b83878338a209fe3c048227a1d5
DDOLDDOL <doyt@naver.com>
2025-09-07 01:47:37 +0900

    refactor: Refactor rtdb queries & mutations

--- lib/number_puzzle/repository/number_puzzle_repository.dart
+++ lib/number_puzzle/repository/number_puzzle_repository.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:rapid_number_puzzle/firebase_options.dart';
@@ -8,30 +9,32 @@ class NumberPuzzleRepository {
     : _database = FirebaseDatabase.instanceFor(
         app: Firebase.app(),
         databaseURL: 'https://mini-games-bf495-default-rtdb.asia-southeast1.firebasedatabase.app',
-      ).ref().child('puzzleRecord');
+      ).ref().child('puzzleRecords');
 
   late final DatabaseReference _database;
 
-  Future<List<PuzzleRecord>> fetchPuzzleRecords(int count) async {
+  Future<List<PuzzleRecord>> fetchPuzzleRecords(int boardSize, int count) async {
     // await Future.delayed(Duration(seconds: 3));
     // throw Exception('Error Error');
 
     try {
-      final snapshot = await _database.orderByChild('clearTime').limitToFirst(count).get();
-      final records = snapshot.children.indexed
-          .map((element) {
-            final (index, child) = element;
-            final id = child.key;
-            final data = {
-              'id': id,
-              'rank': index+1,
-              ...(child.value as Map?)?.cast<String, dynamic>() ?? {},
-            };
+      final snapshot = await _database
+          .child('$boardSize')
+          .orderByChild('clearTime')
+          .limitToFirst(count)
+          .get();
 
-            return PuzzleRecord.fromJson(data);
-          })
-          .toList();
-      return records;
+      return snapshot.children.indexed.map((element) {
+        final (index, child) = element;
+        final id = child.key;
+        final data = {
+          'id': id,
+          'rank': index + 1,
+          ...(child.value as Map?)?.cast<String, dynamic>() ?? {},
+        };
+
+        return PuzzleRecord.fromJson(data);
+      }).toList();
     } on Exception {
       rethrow;
     }
@@ -39,8 +42,8 @@ class NumberPuzzleRepository {
 
   Future<void> savePuzzleRecords(int boardSize, int clearTimeDuration) async {
     try {
-      await _database.push().set({
-        'username': 'abc',
+      await _database.child('$boardSize').push().set({
+        'username': FirebaseAuth.instance.currentUser?.email ?? 'unknown user',
         'boardSize': boardSize,
         'clearTime': clearTimeDuration,
         'clearAt': DateTime.now().toString(),
