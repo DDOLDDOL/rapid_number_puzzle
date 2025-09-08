4c5f76a20ced35fb42c6691941cff175d11cd3ba
DDOLDDOL <28240529+DDOLDDOL@users.noreply.github.com>
2025-09-09 00:58:59 +0900

    refactor: Refactor auth bloc & event & state

--- lib/auth/blocs/auth_event.dart
+++ lib/auth/blocs/auth_event.dart
@@ -2,12 +2,12 @@ part of 'auth_bloc.dart';
 
 @freezed
 class AuthEvent with _$AuthEvent {
-  const factory AuthEvent.oauthSignInRequested({required String oauthProvider,}) =
+  const factory AuthEvent.oauthSignInRequested({required OauthPlatform oauthPlatform}) =
       _OauthSignInRequested;
 
-  const factory AuthEvent.oauthSingOutRequested({required String oauthProvider,}) =
+  const factory AuthEvent.oauthSingOutRequested({required OauthPlatform oauthPlatform}) =
       _OauthSignOutRequested;
 
-  const factory AuthEvent.updateUserRequested({required firebase_auth.User? user,}) =
+  const factory AuthEvent.updateUserRequested({required firebase_auth.User? user}) =
       _UpdateUserRequested;
 }
